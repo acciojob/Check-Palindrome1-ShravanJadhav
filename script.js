25885bcc24f6a058948f12794bfa54f9b4a4be3f
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-05-26 10:07:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // complete the given function
 
 function palindrome(str){
-    let l = 0;
-	let r= str.length;
+ //    let l = 0;
+	// let r= str.length;
 
-	while(l<r){
-		if(str[l]!=str[r])
-			return false;
+	// while(l<r){
+	// 	if(str[l]!=str[r])
+	// 		return false;
 
-		l++;
-		r--;
-	}
-	return true;
-	
+	// 	l++;
+	// 	r--;
+	// }
+	// return true;
+
+	str = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+
+  // Check if the string is a palindrome
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    if (str[i] !== str[j]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 module.exports = palindrome
